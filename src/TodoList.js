ae0edd889797713328ef44f9629e933e7b9ff59c
Derek <nightwing917@hotmail.com>
2022-12-21 19:38:07 -0800

    adding return todo task

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
+import Todo from "./Todo";
 
 class TodoList extends Component {
     constructor(props){
         super(props);
         this.state = {
-            todos: [{ task:"Walk the bird"}, { task:"punch the boss"}]
+            todos: [{ task: "Walk the bird"}, { task: "punch the boss"}]
         };
     }
     render() {
+        const todos = this.state.todos.map(todo => {
+           return <Todo task={todo.task}/>
+        });
         return (
             <div>
                <h1>ToDo List</h1>  
                <ul>
-                 <li>todo 1</li>
-                 <li>todo 2</li>
+                 
                </ul>  
             </div>
         )
