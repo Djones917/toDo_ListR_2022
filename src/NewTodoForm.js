d89a7b7b604850bd68359678c1d240f4683c4422
Derek <nightwing917@hotmail.com>
2022-12-26 19:55:21 -0800

    adding text input

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -3,7 +3,9 @@ import React, from { Component } from "react";
 class NewTodoForm extends Component {
     render() {
         return (
-           <form></form>
+           <form>
+             <input type="text" />
+           </form>
         )
     }
 }
