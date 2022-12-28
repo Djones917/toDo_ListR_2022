d5fedc957cc3c0f05be9f2721400b2f1eb2016d0
Derek <nightwing917@hotmail.com>
2022-12-27 18:53:11 -0800

    added super props to newtodoform js

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,11 +1,15 @@
 import React, from { Component } from "react";
 
 class NewTodoForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {task: ""}
+  }
     render() {
         return (
            <form>
              <label htmlFor="task">New Todo</label>
-             <input type="text" placeholder="New Todo" id="task" />
+             <input type="text" placeholder="New Todo" id="task" value={this.state.task}/>
            </form>
         )
     }
